b644951017efcb232711602e0dd79efba345b10e
Abhijeet Gurle <abhijeetgurle@gmail.com>
2023-02-19 13:55:54 +0530

    feat: add logo

--- components/AppNavbar/index.tsx
+++ components/AppNavbar/index.tsx
@@ -4,16 +4,24 @@ import Navbar from "react-bootstrap/Navbar";
 import Link from "next/link";
 import styles from "./AppNavbar.module.scss";
 import { useRouter } from "next/router";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import Image from "next/image";
 
 export default function AppNavbar() {
   const router = useRouter();
 
+  const onLinkClicked = (e: any, url: string) => {
+    e.stopPropagation();
+    window.open(url, "_blank", "noreferrer");
+  };
+
   return (
-    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+    <Navbar collapseOnSelect expand="lg" bg="black" variant="dark" fixed="top">
       <Container>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto">
+        <Navbar.Collapse id="responsive-navbar-nav" className={styles.navbar}>
+          <Nav>
             <Nav.Link>
               <Link
                 href="/"
@@ -56,7 +64,50 @@ export default function AppNavbar() {
             </Nav.Link>
           </Nav>
           <Nav>
-            <Nav.Link href="#deets">Social</Nav.Link>
+            <Nav.Link>
+              <Link
+                href="/"
+                className={`${styles.link} ${
+                  router.pathname === "/" ? styles.active : ""
+                }`}
+              >
+                <Image
+                  src="/logo.png"
+                  alt="Profile Picture"
+                  width={140}
+                  height={70}
+                />
+              </Link>
+            </Nav.Link>
+          </Nav>
+          <Nav>
+            <Nav.Link>
+              <Link
+                href="https://instagram.com/chitr_agraph?igshid=YmMyMTA2M2Y="
+                className={`${styles.link}`}
+                target="_blank"
+                onClick={(e) =>
+                  onLinkClicked(
+                    e,
+                    "https://instagram.com/chitr_agraph?igshid=YmMyMTA2M2Y="
+                  )
+                }
+              >
+                <InstagramIcon />
+              </Link>
+            </Nav.Link>
+            <Nav.Link>
+              <Link
+                href="https://www.facebook.com/akash.gurle.5"
+                className={`${styles.link}`}
+                target="_blank"
+                onClick={(e) =>
+                  onLinkClicked(e, "https://www.facebook.com/akash.gurle.5")
+                }
+              >
+                <FacebookIcon />
+              </Link>
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
